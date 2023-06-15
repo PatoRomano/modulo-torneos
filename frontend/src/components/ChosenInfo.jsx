199d216405b67bffcb9b892566616304dfdb0d56
Pablo Romano <patoromano22@gmail.com>
2023-06-15 02:02:25 -0300

    chosen info

--- frontend/src/components/ChosenInfo.jsx
+++ frontend/src/components/ChosenInfo.jsx
@@ -11,13 +11,28 @@ const ChosenInfo = ({ precioJugador, precioArbitro }) => {
     return (
         <div className='text-info'>
             <div className="main-text">
-                <h1 className='main-info'>{jsonData.nombreEspacio}</h1>
-                <h1 className='main-info'>{jsonData.instancia}</h1>
+                <h1 className='main-info'>Sede: {jsonData.sede}</h1>
+                <h1 className='main-info'>Espacio: {jsonData.nombreEspacio}</h1>
+                <h1 className='main-info'>Instancia máxima: {jsonData.instancia}</h1>
+                {jsonData.dias && (
+                    <div className="dias-info">
+                        {jsonData.dias.map((dia, index) => (
+                            <div key={index}>
+                                <h3>El día: {dia.dia}</h3>
+                                <div className='time-slots'>
+                                    {dia.horarios.map((horario, index) => (
+                                        <div className='slot' key={index}>{horario}</div>
+                                    ))}
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                )}
             </div>
             <br />
             <div className="text">
-                {precioJugador && <h1 className='info'>Inscripcion por jugador: ${precioJugador}</h1>}
-                {precioArbitro && <h1 className='info'>Arbitro por partido, por jugador: ${precioArbitro}</h1>}
+                {precioJugador && <h1 className='info'>Inscripción por jugador: ${precioJugador}</h1>}
+                {precioArbitro && <h1 className='info'>Árbitro por partido, por jugador: ${precioArbitro}</h1>}
             </div>
         </div>
     );
